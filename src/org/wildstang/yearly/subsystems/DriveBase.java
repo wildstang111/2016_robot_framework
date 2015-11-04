8be4e5e5eee74993f07b99e844c9a3645baf313f
Alex Guerra <aguerra7002@stu.d214.org>
2015-11-04 14:44:39 -0800

    Hopefully successfully added the most basic Victor Test Code ever.

--- src/org/wildstang/yearly/subsystems/DriveBase.java
+++ src/org/wildstang/yearly/subsystems/DriveBase.java
@@ -1,20 +1,41 @@
 package org.wildstang.yearly.subsystems;
 
+import org.wildstang.framework.core.Core;
 import org.wildstang.framework.io.Input;
+import org.wildstang.framework.io.inputs.AnalogInput;
 import org.wildstang.framework.subsystems.Subsystem;
+import org.wildstang.hardware.crio.outputs.WsVictor;
+import org.wildstang.yearly.robot.WSInputs;
 
 public class DriveBase implements Subsystem{
+	WsVictor victor1;
+	WsVictor victor2;
+	Double throttle;
+	
+	public DriveBase(String name) {
 
+	      
+	
+	}
+	
+	
 	@Override
 	public void inputUpdate(Input source) {
 		// TODO Auto-generated method stub
-		
+		if (source.getName() == WSInputs.DRV_BUTTON_1.getName()) {
+			
+		}
+		if (source.getName() == WSInputs.DRV_THROTTLE.getName()) {
+			throttle = ((AnalogInput)source).getValue();
+		}
 	}
 
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
-		
+		Core.getInputManager().getInput(WSInputs.DRV_HEADING.getName()).addInputListener(this);
+	    Core.getInputManager().getInput(WSInputs.DRV_THROTTLE.getName()).addInputListener(this);
+		victor1 = new WsVictor("Test", 0, 0.0);
 	}
 
 	@Override
@@ -26,6 +47,8 @@ public class DriveBase implements Subsystem{
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
+		victor1.setValue(throttle);
+		victor1.sendDataToOutput();
 		
 	}
 
