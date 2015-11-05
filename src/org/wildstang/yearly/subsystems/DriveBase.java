d59ff4a8e02a2baeab7b26b6272f7d074724312d
Steve Garward <garwards@gmail.com>
2015-11-04 20:24:47 -0600

    Small corrections

--- src/org/wildstang/yearly/subsystems/DriveBase.java
+++ src/org/wildstang/yearly/subsystems/DriveBase.java
@@ -3,59 +3,60 @@ package org.wildstang.yearly.subsystems;
 import org.wildstang.framework.core.Core;
 import org.wildstang.framework.io.Input;
 import org.wildstang.framework.io.inputs.AnalogInput;
+import org.wildstang.framework.io.outputs.AnalogOutput;
 import org.wildstang.framework.subsystems.Subsystem;
-import org.wildstang.hardware.crio.outputs.WsVictor;
 import org.wildstang.yearly.robot.WSInputs;
+import org.wildstang.yearly.robot.WSOutputs;
 
-public class DriveBase implements Subsystem{
-	WsVictor victor1;
-	WsVictor victor2;
-	Double throttle;
-	
-	public DriveBase(String name) {
+public class DriveBase implements Subsystem
+{
+   // WsVictor victor1;
+   // WsVictor victor2; NO - new framework doesn't require this
+   Double throttle;
 
-	      
-	
-	}
-	
-	
-	@Override
-	public void inputUpdate(Input source) {
-		// TODO Auto-generated method stub
-		if (source.getName() == WSInputs.DRV_BUTTON_1.getName()) {
-			
-		}
-		if (source.getName() == WSInputs.DRV_THROTTLE.getName()) {
-			throttle = ((AnalogInput)source).getValue();
-		}
-	}
+   public DriveBase(String name)
+   {
 
-	@Override
-	public void init() {
-		// TODO Auto-generated method stub
-		Core.getInputManager().getInput(WSInputs.DRV_HEADING.getName()).addInputListener(this);
-	    Core.getInputManager().getInput(WSInputs.DRV_THROTTLE.getName()).addInputListener(this);
-		victor1 = new WsVictor("Test", 0, 0.0);
-	}
+   }
 
-	@Override
-	public void selfTest() {
-		// TODO Auto-generated method stub
-		
-	}
+   @Override
+   public void inputUpdate(Input source)
+   {
+      if (source.getName() == WSInputs.DRV_BUTTON_1.getName())
+      {
 
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-		victor1.setValue(throttle);
-		victor1.sendDataToOutput();
-		
-	}
+      }
+      if (source.getName() == WSInputs.DRV_THROTTLE.getName())
+      {
+         throttle = ((AnalogInput) source).getValue();
+      }
+   }
 
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   @Override
+   public void init()
+   {
+      Core.getInputManager().getInput(WSInputs.DRV_HEADING.getName()).addInputListener(this);
+      Core.getInputManager().getInput(WSInputs.DRV_THROTTLE.getName()).addInputListener(this);
+   }
+
+   @Override
+   public void selfTest()
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void update()
+   {
+      ((AnalogOutput) Core.getOutputManager().getOutput(WSOutputs.VICTOR.getName())).setValue(throttle);
+
+   }
+
+   @Override
+   public String getName()
+   {
+      return "Drive Base";
+   }
 
 }
