aa73920ed1cbc1397a38206c7d7f51064a11534d
Steve Garward <garwards@gmail.com>
2015-11-11 20:53:47 -0600

    Added Swerve branch

---
+++ src/org/wildstang/yearly/subsystems/SwerveDrive.java
@@ -0,0 +1,44 @@
+package org.wildstang.yearly.subsystems;
+
+import org.wildstang.framework.io.Input;
+import org.wildstang.framework.subsystems.Subsystem;
+
+public class SwerveDrive implements Subsystem
+{
+
+   @Override
+   public void inputUpdate(Input p_source)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void init()
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void selfTest()
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void update()
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public String getName()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+}
