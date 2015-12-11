4387b3687987febf12bfa244aec4a2145b1c912b
Steve Garward <garwards@gmail.com>
2015-12-11 15:37:55 -0600

    Updated to send teleop data

--- src/org/wildstang/yearly/subsystems/LED.java
+++ src/org/wildstang/yearly/subsystems/LED.java
@@ -2,14 +2,13 @@ package org.wildstang.yearly.subsystems;
 
 import org.wildstang.framework.core.Core;
 import org.wildstang.framework.io.Input;
+import org.wildstang.framework.io.inputs.DigitalInput;
 import org.wildstang.framework.subsystems.Subsystem;
 import org.wildstang.hardware.crio.outputs.WsI2COutput;
 import org.wildstang.yearly.robot.SwerveInputs;
 import org.wildstang.yearly.robot.WSOutputs;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.I2C.Port;
 
 /**
  *
@@ -19,34 +18,15 @@ public class LED implements Subsystem
 
    // Sent states
    boolean autoDataSent = false;
+   boolean m_newDataAvailable = false;
    boolean disableDataSent = false;
    boolean sendData = false;
 
    private String m_name;
-   
+
    WsI2COutput m_ledOutput;
 
-   public static class LedCmd
-   {
-
-      byte[] dataBytes = new byte[5];
-
-      public LedCmd(int command, int payloadByteOne, int payloadByteTwo)
-      {
-
-         dataBytes[0] = (byte) command;
-         dataBytes[1] = (byte) payloadByteOne;
-         dataBytes[2] = (byte) payloadByteTwo;
-         // Extremely fast and cheap data confirmation algorithm
-         dataBytes[3] = (byte) (~dataBytes[1]);
-         dataBytes[4] = (byte) (~dataBytes[2]);
-      }
-
-      byte[] getBytes()
-      {
-         return dataBytes;
-      }
-   }
+   boolean m_antiTurbo;
 
    /*
     * | Function | Cmd | PL 1 | PL 2 |
@@ -83,8 +63,8 @@ public class LED implements Subsystem
       Core.getInputManager().getInput(SwerveInputs.ANTI_TURBO.getName()).addInputListener(this);
       Core.getInputManager().getInput(SwerveInputs.TURBO.getName()).addInputListener(this);
       Core.getInputManager().getInput(SwerveInputs.DRV_BUTTON_2.getName()).addInputListener(this);
-      
-      m_ledOutput = (WsI2COutput)Core.getOutputManager().getOutput(WSOutputs.LED.getName());
+
+      m_ledOutput = (WsI2COutput) Core.getOutputManager().getOutput(WSOutputs.LED.getName());
    }
 
    @Override
@@ -100,7 +80,15 @@ public class LED implements Subsystem
       {
          if (isRobotTeleop)
          {
-            // Do nothing, handled in acceptNotification
+            if (m_newDataAvailable)
+            {
+               if (m_antiTurbo)
+               {
+                  m_ledOutput.setValue(climbCmd.getBytes());
+               }
+               
+               m_newDataAvailable = false;
+            }
          }
          else if (isRobotAuton)
          {
@@ -154,22 +142,12 @@ public class LED implements Subsystem
    @Override
    public void inputUpdate(Input source)
    {
-      // if (((DigitalInput)
-      // source).getName().equals(WSInputs.MAN_BUTTON_6.getName()))
-      // {
-      // if (((DigitalInput) source).getValue())
-      // {
-      // sendData(shootCmd);
-      // }
-      // }
-      // else if (((DigitalInput)
-      // source).getName().equals(WSInputs.DRV_BUTTON_2.getName()))
-      // {
-      // if (((DigitalInput) source).getValue())
-      // {
-      // sendData(climbCmd);
-      // }
-      // }
+      if (source.getName().equals(SwerveInputs.ANTI_TURBO.getName()))
+      {
+         m_antiTurbo = ((DigitalInput) source).getValue();
+      }
+      
+      m_newDataAvailable = true;
    }
 
    @Override
@@ -183,4 +161,25 @@ public class LED implements Subsystem
       return m_name;
    }
 
+   public static class LedCmd
+   {
+
+      byte[] dataBytes = new byte[5];
+
+      public LedCmd(int command, int payloadByteOne, int payloadByteTwo)
+      {
+
+         dataBytes[0] = (byte) command;
+         dataBytes[1] = (byte) payloadByteOne;
+         dataBytes[2] = (byte) payloadByteTwo;
+         // Extremely fast and cheap data confirmation algorithm
+         dataBytes[3] = (byte) (~dataBytes[1]);
+         dataBytes[4] = (byte) (~dataBytes[2]);
+      }
+
+      byte[] getBytes()
+      {
+         return dataBytes;
+      }
+   }
 }
