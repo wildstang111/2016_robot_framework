46bee569d3d4c53e74c0d8e7a9ccd7ba59c88232
wdoherty <billydoherty5104@gmail.com>
2016-04-14 19:25:04 -0500

    vision changes

--- src/org/wildstang/yearly/subsystems/Vision.java
+++ src/org/wildstang/yearly/subsystems/Vision.java
@@ -3,7 +3,6 @@ package org.wildstang.yearly.subsystems;
 import org.wildstang.framework.core.Core;
 import org.wildstang.framework.io.Input;
 import org.wildstang.framework.io.inputs.RemoteAnalogInput;
-import org.wildstang.framework.io.inputs.RemoteDigitalInput;
 import org.wildstang.framework.subsystems.Subsystem;
 import org.wildstang.yearly.robot.WSInputs;
 
@@ -11,66 +10,64 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Vision implements Subsystem
 {
-//   private double distanceToTarget;
-//   private double angleToRotateX;
-//   private double angleToRotateY;
-//   private double angleToRotateZ;
-//   private double angleToRotate;
-//   private boolean isOnTarget;
-   private int rotateInt = 7;
-   private int rotateIntTele = 8;
-   private boolean goodHeight = false;
-   
+   private final double WIDTH = 640;
+   private final double HEIGHT = 480;
+
+   private final int CENTERED_LEFT = 310;
+   private final int CENTERED_RIGHT = 330;
+   private final int BOTTOM_LOW_MIN = 45;
+   private final int BOTTOM_LOW_MAX = 70;
+   private final int BOTTOM_HIGH_MIN = 130;
+   private final int BOTTOM_HIGH_MAX = 152;
+
+   private int m_centered_left;
+   private int m_centered_right;
+   private int m_bottom_low_min;
+   private int m_bottom_low_max;
+   private int m_bottom_high_min;
+   private int m_bottom_high_max;
+
+   private int targetBottom = 7;
+   private int targetCenter = 8;
+   private double robotAngle;
+   private double robotDistance;
+   private int rotateInt = 6;
+
+   private boolean onTarget = false;
+   private boolean goodLongHeight = false;
+   private boolean goodShortHeight = false;
+
    @Override
    public void inputUpdate(Input source)
    {
       // TODO Auto-generated method stub
-      if (source.getName().equals(WSInputs.ROTATION_INTEGER.getName())) {
-         rotateInt = (int) ((RemoteAnalogInput) source).getValue();
+      if (source.getName().equals(WSInputs.TARGET_BOTTOM.getName()))
+      {
+         targetBottom = (int) ((RemoteAnalogInput) source).getValue();
       }
-      if (source.getName().equals(WSInputs.ROTATION_INTEGER_TELEOP.getName())) {
-         rotateIntTele = (int) ((RemoteAnalogInput) source).getValue();
+      if (source.getName().equals(WSInputs.TARGET_CENTER.getName()))
+      {
+         targetCenter = (int) ((RemoteAnalogInput) source).getValue();
       }
-      if (source.getName().equals(WSInputs.CAMERA_HEIGHT.getName())) {
-         goodHeight = (boolean) ((RemoteDigitalInput) source).getValue();
+      if (source.getName().equals(WSInputs.VISION_ANGLE.getName()))
+      {
+         robotAngle = ((RemoteAnalogInput) source).getValue();
       }
-      
-//      if (source.getName().equals(WSInputs.CAMERA_DISTANCE.getName())) {
-//         distanceToTarget = ((RemoteAnalogInput) source).getValue();
-//      } else if (source.getName().equals(WSInputs.CAMERA_ANGLE_X.getName())) {
-//         angleToRotateX = ((RemoteAnalogInput) source).getValue();
-//      } else if (source.getName().equals(WSInputs.CAMERA_ANGLE_Y.getName())) {
-//         angleToRotateY = ((RemoteAnalogInput) source).getValue();
-//      } else if (source.getName().equals(WSInputs.CAMERA_ANGLE_Z.getName())) {
-//         angleToRotateZ = ((RemoteAnalogInput) source).getValue();
-//      } else if (source.getName().equals(WSInputs.ON_TARGET.getName())) {
-//         isOnTarget = ((RemoteDigitalInput) source).getValue();
-//      }
-//      else if (source.getName().equals(WSInputs.ANGLE_OF_ROTATION.getName())) {
-//         angleToRotate = ((RemoteAnalogInput) source).getValue();
-//      }
+      if (source.getName().equals(WSInputs.VISION_DISTANCE.getName()))
+      {
+         robotDistance = ((RemoteAnalogInput) source).getValue();
+      }
+
    }
 
    @Override
    public void init()
    {
-      // TODO Auto-generated method stub
-//      distanceToTarget = 0;
-//      angleToRotateX = 0;
-//      angleToRotateY = 0;
-//      angleToRotateZ = 0;
-//      angleToRotate = 0;
-//      isOnTarget = false;
 
-//      Core.getInputManager().getInput(WSInputs.CAMERA_ANGLE_X.getName()).addInputListener(this);
-//      Core.getInputManager().getInput(WSInputs.CAMERA_ANGLE_Y.getName()).addInputListener(this);
-//      Core.getInputManager().getInput(WSInputs.CAMERA_ANGLE_Z.getName()).addInputListener(this);
-//      Core.getInputManager().getInput(WSInputs.ANGLE_OF_ROTATION.getName()).addInputListener(this);
-//      Core.getInputManager().getInput(WSInputs.CAMERA_DISTANCE.getName()).addInputListener(this);
-//      Core.getInputManager().getInput(WSInputs.ON_TARGET.getName()).addInputListener(this);
-      Core.getInputManager().getInput(WSInputs.ROTATION_INTEGER.getName()).addInputListener(this);
-      Core.getInputManager().getInput(WSInputs.ROTATION_INTEGER_TELEOP.getName()).addInputListener(this);
-      Core.getInputManager().getInput(WSInputs.CAMERA_HEIGHT.getName()).addInputListener(this);
+      Core.getInputManager().getInput(WSInputs.TARGET_BOTTOM.getName()).addInputListener(this);
+      Core.getInputManager().getInput(WSInputs.TARGET_CENTER.getName()).addInputListener(this);
+      Core.getInputManager().getInput(WSInputs.VISION_ANGLE.getName()).addInputListener(this);
+      Core.getInputManager().getInput(WSInputs.VISION_DISTANCE.getName()).addInputListener(this);
    }
 
    @Override
@@ -82,16 +79,56 @@ public class Vision implements Subsystem
    @Override
    public void update()
    {
-      boolean onTarget = ((rotateIntTele == 0) ? true : false);
-      
-      // TODO Auto-generated method stub
+
+      m_centered_left = (int) SmartDashboard.getNumber("Left Centered Mark", CENTERED_LEFT);
+      m_centered_right = (int) SmartDashboard.getNumber("Right Centered Mark", CENTERED_RIGHT);
+      m_bottom_low_min = (int) SmartDashboard.getNumber("Target Bottom Low Min", BOTTOM_LOW_MIN);
+      m_bottom_low_max = (int) SmartDashboard.getNumber("Target Bottom Low Min", BOTTOM_LOW_MAX);
+      m_bottom_high_min = (int) SmartDashboard.getNumber("Target Bottom Low Min", BOTTOM_HIGH_MIN);
+      m_bottom_high_max = (int) SmartDashboard.getNumber("Target Bottom Low Min", BOTTOM_HIGH_MAX);
+
+      if (targetBottom > m_bottom_low_min && targetBottom < m_bottom_low_max)
+      {
+         goodLongHeight = true;
+      }
+      else
+      {
+         goodLongHeight = false;
+      }
+
+      if (targetBottom > m_bottom_high_min && targetBottom < m_bottom_high_max)
+      {
+         goodShortHeight = true;
+      }
+      else
+      {
+         goodShortHeight = false;
+      }
+
+      if (targetCenter > m_centered_left && targetCenter < m_centered_right)
+      {
+         rotateInt = 0;
+      }
+      else if (m_centered_left >= targetCenter)
+      {
+         rotateInt = -1;
+      }
+      else
+      {
+         rotateInt = 1;
+      }
+
+      onTarget = ((rotateInt == 0) ? true : false);
       SmartDashboard.putNumber("Rotation Integer", rotateInt);
-      boolean xTarget= rotateIntTele == 0 ? true : false;
+      SmartDashboard.putNumber("TargetBottom", targetBottom);
+      boolean xTarget = rotateInt == 0 ? true : false;
       SmartDashboard.putBoolean("On Target X", xTarget);
-      SmartDashboard.putBoolean("On Target Y", goodHeight);
-//      SmartDashboard.putNumber("Camera Distance", distanceToTarget);
-//      SmartDashboard.putNumber("Camera Angle", angleToRotate);
-//      SmartDashboard.putBoolean("On Target?", isOnTarget);
+      SmartDashboard.putBoolean("On Target Y", goodLongHeight
+            || goodShortHeight);
+      // SmartDashboard.putNumber("Camera Distance", distanceToTarget);
+      // SmartDashboard.putNumber("Camera Angle", angleToRotate);
+      // SmartDashboard.putBoolean("On Target?", isOnTarget);
+
    }
 
    @Override
@@ -100,29 +137,25 @@ public class Vision implements Subsystem
       // TODO Auto-generated method stub
       return "Vision";
    }
-   
-   public int getRotateInt() {
+
+   public int getRotateInt()
+   {
       return rotateInt;
    }
-   
-//   public double getAngleToRotateX() {
-//      return angleToRotateX;
-//   }
-//   
-//   public double getAngleToRotateY() {
-//      return angleToRotateY;
-//   }
-//   
-//   public double getAngleToRotateZ() {
-//      return angleToRotateZ;
-//   }
-//   
-//   public double getDistanceToTarget() {
-//      return distanceToTarget;
-//   }
-//   
-//   public boolean getOnTarget() {
-//      return isOnTarget;
-//   }
+
+   public double getDistanceToTarget()
+   {
+      return robotDistance;
+   }
+
+   public double getAngleToTarget()
+   {
+      return robotAngle;
+   }
+
+   public boolean getOnTarget()
+   {
+      return onTarget;
+   }
 
 }
